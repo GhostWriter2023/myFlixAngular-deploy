133ffde77325fb4b34fc9186d95a402aeee9d976
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-06-26 16:29:06 -0400

    Project Initialization
    -Installed @angular/cli globally
    -Created an Angular app project folder
    -Created service files
    -Coded logic for making API calls
    -Drafted README file

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # MyFlixAngular
 
+## Objective
+To build the client-side for an application called MyFlixAngular based on myFlix app and its existing server-side code (REST API and database), with supporting documentation, using Angular.
+
+## Context
+As remote work becomes more important, effective communication and collaboration are crucial. This project aims to enhance collaboration skills by creating detailed handoffs for other developers. Building the client-side interface for the MyFlixAngular app using Angular will provide valuable experience in working with a widely-used framework for large projects.
+
+## Project Overview & Requirements
+The following sections provide information about the requirements for the app.
+
+## The 5 W’s
+**Who** — The users of the MyFlixAngular movie app and codebase, including other developers and designers.
+
+**What** — A single-page, responsive movie app built with Angular, facilitating user requests and rendering responses from the server-side. Accompanied by relevant documentation and handoff deliverables.
+
+**When** — Users can use the app whenever they want to read information about different movies or update their user information.
+
+**Where** — The app will be hosted online, accessible from any device.
+
+**Why** — Movie enthusiasts seek easy access to movie information. The app demonstrates Angular skills and ability to create documentation for developers and employers.
+
+## Design Criteria
+### User Stories
+- As a user, I want to receive information on movies, directors, and genres to learn more about movies.
+- As a user, I want to create a profile to save data about my favorite movies.
+
+## Key Features
+- Welcome view for login/registration.
+- View all movies upon authentication.
+- Single movie view with additional features:
+  - Button to view director details.
+  - Button to view genre details.
+
+## Technical Requirements
+- Written in Angular (version 9 or later).
+- Requires the latest version of Node.js and npm package.
+- Contains user registration and login forms.
+- Designed using Angular Material.
+- Codebase contains comments using Typedoc.
+- Project contains technical documentation using JSDoc.
+- Hosted on GitHub Pages.
+
+## Technologies Used for this Project
+- Angular @angular/cli v.18.0.5
+- Node.js Modules v.18.19.0
+- npm v10.2.3
+- JavaScript (ES2015+)
+- 
+- Various middleware packages & Package Managers
+
+## Getting Started
+
+The application is accessible at GitHub at the following link:
+   [https://github.com/GhostWriter2023/MyFlixAngular](https://github.com/GhostWriter2023/MyFlixAngular)
+
+### Setting Up the Development Environment
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.5.
 
 ## Development server
@@ -25,3 +81,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+## Author
+GhostWriter
---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,182 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { title } from 'node:process';
+
+//Declaring the api url that will provide data for the client app
+const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiDataCallService {
+  // Inject the HttpClient module to the constructor params
+ // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {
+  }
+
+  // Method making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call for the user login endpoint
+  public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'login', userDetails).pipe(
+    catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to get all movies from the myFlix API
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+  // Non-typed response extraction
+  private extractResponseData(res: Response): any {
+    const body = res;
+    return body || { };
+  }
+
+  // Method making the api call to get one movie from the myFlix API
+  getOneMovie(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies/' + title, {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to get a director by name from the myFlix API
+  getDirector(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies/director/:Name', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to get the movies for a specified genre from the myFlix API
+  getGenre(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies/genre/:Name', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to get a director by name from the myFlix API
+  getUser(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to get a user's favourite movies from the myFlix API
+  getFavouriteMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username/movies', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to add a movie to a user's favourites
+  addFavouriteMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username/movies', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to edit a user's info
+  editUser(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to delete a user
+  deleteUser(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method making the api call to delete a movie from the user's favorite movies
+  deleteFavouriteMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'username/movies/:movie.title', {headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+    console.error('Some error occurred:', error.error.message);
+    } else {
+    console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+    'Something bad happened; please try again later.');
+  }
+}
+
